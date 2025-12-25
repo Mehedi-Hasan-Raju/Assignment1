0bf869732f18404f15a49dd1f200fb26c8824284
Mehedi Hasan Raju <145052408+Mehedi-Hasan-Raju@users.noreply.github.com>
2025-12-25 13:44:23 +0600

    Update solution.ts

--- solution.ts
+++ solution.ts
@@ -1,1 +1,12 @@
-
+function formatValue(value: string | number | boolean) : string | number | boolean {
+    if (typeof value === "string") {
+        return value.toUpperCase();
+    }
+    else if (typeof value === "number") {
+        return value * 10;
+    }
+    else {
+        return !value;
+    }
+    
+}
